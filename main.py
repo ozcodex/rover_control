04d524e31ef904f2ea96c6a891bb437679afbd31
Oscar Bernal <oscarbernal93@gmail.com>
2020-12-24 09:08:16 -0500

    Replace Movement with pygame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import requests
-import tkinter as tk
-
-pressed_key = 'none';
-relesed_key = 'none';
+import pygame
 
 def call_action():
         try:
@@ -11,23 +8,42 @@ def call_action():
         except:
             print("Error!")
 
-window = tk.Tk()
+pygame.init()
+pygame.display.set_caption('Rover Control')
 
-def key_pressed(event):
-    global pressed_key
-    if (pressed_key != event.keysym):
-        pressed_key = event.keysym
-        print(pressed_key)
-        if(pressed_key == 'Escape'):
-            window.destroy()
-    
-def key_relesed(event):
-    global relesed_key
-    if (relesed_key != event.keysym):
-        relesed_key = event.keysym
-        print("keyRelese:"+ relesed_key)
+screen = pygame.display.set_mode((720, 480))
+clock = pygame.time.Clock()
+FPS = 60
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-window.bind("<Key>",key_pressed)
-window.bind("<KeyRelease>",key_relesed)
+rect = pygame.Rect((344, 224), (32, 32))
+image = pygame.Surface((32, 32))
+image.fill(WHITE)
 
-window.mainloop()
+while True:
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        elif event.type == pygame.KEYDOWN:
+            rect.x = 344
+            rect.y = 224
+            if event.key == pygame.K_UP:
+                rect.y -= 16
+            elif event.key == pygame.K_DOWN:
+                rect.y += 16
+            elif event.key == pygame.K_LEFT:
+                rect.x -= 16
+            elif event.key == pygame.K_RIGHT:
+                rect.x += 16
+            elif event.key == pygame.K_ESCAPE:
+                quit()
+        elif event.type == pygame.KEYUP:
+            rect.x = 344
+            rect.y = 224
+
+    screen.fill(BLACK)
+    screen.blit(image, rect)
+    pygame.display.update()
