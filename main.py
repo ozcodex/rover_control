5c5f0ddf2a58a1cf04a76335991422282e26cd4d
Oscar Bernal <oscarbernal93@gmail.com>
2020-12-24 12:01:08 -0500

    Add Threads to manage HTTP requests

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import requests
 import pygame
+import threading
 
 def call_action(action='stop'):
         try:
-            response = requests.get('http://192.168.20.38/action='+action, timeout=1)
+            response = requests.get('http://192.168.20.38/action='+action, timeout=2)
             print("Success!")
         except:
             print("Error!")
 
-
 def close():
     call_action('stop')
     quit()
+
+######### MAIN ########
+
+
 
 pygame.init()
 pygame.display.set_caption('Rover Control')
@@ -33,24 +37,24 @@ while True:
         if event.type == pygame.QUIT:
             close()
         elif event.type == pygame.KEYDOWN:
+            action="stop"
             rect.x = 344
             rect.y = 224
             if event.key == pygame.K_UP:
                 rect.y -= 16
-                call_action('forward')
+                threading.Thread(name='action_daemon', target=call_action, daemon=True, args=('forward',)).start()
             elif event.key == pygame.K_DOWN:
-                rect.y += 16
-                call_action('backward')
+                rect.y += 16               
+                threading.Thread(name='action_daemon', target=call_action, daemon=True, args=('forward',)).start()
             elif event.key == pygame.K_LEFT:
                 rect.x -= 16
-                call_action('left')
+                threading.Thread(name='action_daemon', target=call_action, daemon=True, args=('forward',)).start()
             elif event.key == pygame.K_RIGHT:
                 rect.x += 16
-                call_action('right')
+                threading.Thread(name='action_daemon', target=call_action, daemon=True, args=('forward',)).start()
             elif event.key == pygame.K_ESCAPE:
                 close()
         elif event.type == pygame.KEYUP:
-            call_action('stop')
             rect.x = 344
             rect.y = 224
 
